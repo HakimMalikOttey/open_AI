f5a556b532692304fbe65a4aec2ec48742473b5f
HakimMalikOttey <35748681+HakimMalikOttey@users.noreply.github.com>
2019-06-09 13:16:16 -0700

    Add files via upload

--- Ob_Detect.py
+++ Ob_Detect.py
@@ -1,47 +1,47 @@
 import cv2
-from imageai.Detection import ObjectDetection
+#from picamera.array import PiGBArray
+from imageai.Detection import VideoObjectDetection
 import os
+import argparse
 import imutils
 from imutils.video import FPS
-cascPath = "haarcascade_frontalface_default.xml"
-video_capture = cv2.VideoCapture(0)
+
 execution_path = os.getcwd()
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector = VideoObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
 detector.loadModel()
-index = 0
-timer = 0
+video_capture = cv2.VideoCapture(0)
+#construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default= 300, help="minimum area size")
+args = vars(ap.parse_args())
+# initialize the first frame in the video stream. Do no place in While True, as that would deactivate object recognition
+firstframe = None
 while True:
-    timer = timer + 1
-    index = index + 1
     fps = FPS().start()
     ret,frame = video_capture.read()
-    name = str(index) + '.jpg'
-    cv2.imwrite(name, frame)
-    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, name), output_image_path=os.path.join(execution_path, name))
-    faceCascade = cv2.CascadeClassifier(cascPath)
+    #Window size and image blur
+    frame = imutils.resize(frame, width = 400)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor= 1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-)
-    if timer == 100000000000000000000000000000000000:
-        name = str(index) + '.jpg'
-        cv2.imwrite(name, frame)
-        detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, name),output_image_path=os.path.join(execution_path, name))
-        timer = 0
-        index = index + 1
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        fps.update()
-    #print("[INFO] elapsed time:[:.2f}".format(fps.elapsed()))
-    #print("[INFO] approx.FPS:[:.2f}".format(fps.fps()))
-    cv2.imshow("Video", frame)
+    gray = cv2.GaussianBlur(gray,(21,21),0)
+    #If the first frame is none, initialize it
+    if firstframe is None:
+        firstframe = gray
+        continue
+    frameDelta = cv2.absdiff(firstframe,gray)
+    thresh = cv2.threshold(frameDelta,25,255,cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh,None,iterations=2)
+    contour = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contour = imutils.grab_contours(contour)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-video_capture.release()
-cv2.destroyAllWindows()
+    for c in contour:
+        #If countour is too small
+        if cv2.contourArea(c) < args["min_area"]:
+            continue
+        # draws box around detected object
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame,(x,y), ( x + w, y +h), (0,225,0),2)
+        #detections = detector.detectObjectsFromVideo(camera_input = video_capture,output_file_path=os.path.join(execution_path, "camera_detected_1"),frames_per_second=29, log_progress=True)
+    cv2.imshow("Video", frame)
